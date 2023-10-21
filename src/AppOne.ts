3a8714c3b3459150d80f76676c86eae05f5732cb
ZingerEngineer <ramyisez@gmail.com>
2023-10-21 19:06:58 +0300

    circle drawing WIP

--- src/AppOne.ts
+++ src/AppOne.ts
@@ -45,31 +45,63 @@ var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
 
   light.intensity = 0.7
 
-  var blueMaterial = new BABYLON.StandardMaterial('Blue Material', scene)
-  blueMaterial.alpha = 1
-  blueMaterial.diffuseColor = new BABYLON.Color3(0, 0, 1)
+  var yellowMaterial = new BABYLON.StandardMaterial('Yellow Material', scene)
+  yellowMaterial.alpha = 1
+  yellowMaterial.diffuseColor = new BABYLON.Color3(1, 1, 0)
 
-  var redMaterial = new BABYLON.StandardMaterial('Red Material', scene)
-  redMaterial.alpha = 1
-  redMaterial.diffuseColor = new BABYLON.Color3(1.0, 0, 0)
-
-  var redBox = BABYLON.MeshBuilder.CreateBox(
-    'red-box',
+  var YellowSphere = BABYLON.MeshBuilder.CreateSphere(
+    'yellow-sphere',
     {
-      width: 3,
-      height: 3
+      diameterX: 1,
+      diameterY: 0.8,
+      diameterZ: 1
     },
     scene
   )
-  redBox.material = redMaterial
-  var blueBox = BABYLON.MeshBuilder.CreateBox(
-    'blue-box',
-    {
-      width: 6,
-      height: 3
-    },
-    scene
-  )
-  blueBox.material = blueMaterial
+  YellowSphere.material = yellowMaterial
+  //(x−x0)2+(y−y0)2+(z−z0)2=r2
+  //   const meshInstance = (meshToDublicate: BABYLON.Mesh) => {
+  //     for (let i = 0; i < 100; i++) {
+  //       let newInstance = meshToDublicate.createInstance(meshToDublicate.name + i)
+
+  //       //   newInstance.rotation.x = meshToDublicate.rotation.x + i / 10
+  //       //   newInstance.rotation.y = meshToDublicate.rotation.y + i / 10
+  //       //   newInstance.rotation.z = meshToDublicate.rotation.z + i / 10
+
+  //       newInstance.position.x = meshToDublicate.position.x + i / 10
+  //       newInstance.position.y = meshToDublicate.position.y + i / 10
+  //       newInstance.position.z = meshToDublicate.position.z + i / 10
+  //     }
+  //   }
+  //   meshInstance(YellowSphere)
+  const arrayRange = (start: number, stop: number, step: number) =>
+    Array.from(
+      { length: (stop - start) / step + 1 },
+      (value, index) => start + index * step
+    )
+  const twoDeeCircleDraw = (
+    meshToDublicate: BABYLON.Mesh,
+    diameter: number
+  ) => {
+    if (diameter <= 0) throw new Error('Inavlid diameter value.')
+    const coordinatesYArrayHalf1 = arrayRange(-diameter / 2, 0 / 2, 0.5)
+    const coordinatesYArrayHalf2 = arrayRange(1 / 2, diameter / 2, 0.5)
+    const coordinatesYArray = [
+      ...coordinatesYArrayHalf1,
+      ...coordinatesYArrayHalf2
+    ]
+    console.log(coordinatesYArray)
+
+    for (let i = 0; i < coordinatesYArray.length - 1; i++) {
+      let newInstance = meshToDublicate.createInstance(meshToDublicate.name + i)
+      let newYCoordinate = coordinatesYArray[i]
+      newInstance.position.y = newYCoordinate
+      newInstance.position.x = Math.sqrt(
+        Math.pow(diameter, 2) - Math.pow(newYCoordinate, 2)
+      )
+      console.log(Math.pow(diameter, 2) - Math.pow(newYCoordinate, 2))
+    }
+  }
+  twoDeeCircleDraw(YellowSphere, 10)
   return scene
 }
